bb04b339b41eb33979e1527866dbd6d049ee245b
argentoaskia <askiaxiaoming@outlook.com>
2022-02-26 01:05:45 +0800

    解决html页面无法显示头像问题

--- src/main/java/tech/argentoaskia/bookzone/model/beans/atomic/Icon.java
+++ src/main/java/tech/argentoaskia/bookzone/model/beans/atomic/Icon.java
@@ -1,42 +1,47 @@
 package tech.argentoaskia.bookzone.model.beans.atomic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.ClassUtils;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.*;
 
-public class Icon implements Type<Image>, Serializable {
+public class Icon implements Type<RenderedImage>, Serializable {
 
 
     private static final long serialVersionUID = 3669359192670864363L;
+    private static final Logger logger = LoggerFactory.getLogger(Icon.class);
 
-    private transient Image image;
+    public final String root = ClassUtils.getDefaultClassLoader().getResource("").getPath();
+    public final String staticFolder = "static/";
+    public final String default_url = "img/icon/随机用户.jpg";
+    // 注意，这个对象无法序列化，因此无法存储在Session中
+    private transient RenderedImage image;
     private File file;
 
     public String getImageURL() {
-        return file.getPath();
+        System.out.println("root: " + root);
+        return root + staticFolder + file.getPath();
     }
 
     public void setImage(String url) throws IOException {
-        String path = ClassUtils.getDefaultClassLoader().getResource("").getPath();
-        System.out.println(path);
-        final String staticFile = "static/";
-        final String default_url = "img/icon/default.png";
-        try {
-            file = new File(path + staticFile + url);
-            FileInputStream fileInputStream = new FileInputStream(file);
-            image = ImageIO.read(fileInputStream);
-        } catch (FileNotFoundException e) {
-            // todo use default icon
-            file = new File(path + staticFile +default_url);
-            image = ImageIO.read(file);
+        if (url == null || url.equals("")) {
+            file = new File(default_url);
+            return;
         }
+        file = new File(url);
     }
 
     @Override
-    public Image get() {
+    public RenderedImage get() {
+        File realFile = new File(root + staticFolder + file);
+        try {
+            image = ImageIO.read(realFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return image;
     }
 }
